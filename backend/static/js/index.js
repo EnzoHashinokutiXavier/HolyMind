1896b19996ccf4caff71abea4f5845599580cc19
Augustbr01 <augustogatticorrea777@gmail.com>
2025-08-23 02:08:25 -0400

    fix: adjust error words in sendRequest function

--- backend/static/js/index.js
+++ backend/static/js/index.js
@@ -1,28 +1,28 @@
-async function sendRequest(type) {
-    const entrada = document.getElementById('entrada').value.trim();
+async function sendRequest(type) { // Função para requisitar a resposta da IA, mandando a entrada do usuario para a API
+    const entrada = document.getElementById('entrada').value.trim(); 
     const respostaIA = document.getElementById('resposta');
 
     if (!entrada) {
-        respostaIA.innerText = "DIGITA ALGUMA COISA AI NÉ CARA";
+        respostaIA.innerText = "DIGITA ALGUMA COISA AI NÉ CARA"; // Texto para caso o usuario não digite nada
         return;
     }
 
-    respostaIA.innerText = "Carregando espera ai...";
+    respostaIA.innerText = "Carregando espera ai..."; // Texto para loading
 
     try {
-        const resp = await fetch(`/${type}`, {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({entrada: entrada})
+        const resp = await fetch(`/${type}`, { // mandando a requisição
+            method: "POST", // tipo da requisição
+            headers: {"Content-Type": "application/json"}, // tipo de conteudo da requisição
+            body: JSON.stringify({text: entrada}) // corpo em json do conteudo a ser recebido da API
         });
 
         if (resp.ok) { // Se a resposta da api for OK
             const data = await resp.json(); // Data vai esperar um json
             respostaIA.innerText = data.explanation; // O json vai ser mostrado no html diretamente na div de ID resposta da IA
         } else { 
-            const DataErro = await resp.json(); // Caso responda fora da condição, o erro esperado ser gerado vai ser exibido na div de ID resposta
-            respostaIA.innerText = errorData.detail
-                ? `Erro: ${errorData.detail}`
+            const dataErro = await resp.json(); // Caso responda fora da condição, o erro esperado ser gerado vai ser exibido na div de ID resposta
+            respostaIA.innerText = dataErro.detail
+                ? `Erro: ${dataErro.detail}`
                 : "Error fetching explanation.";
         }  
     } catch (error) {

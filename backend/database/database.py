661398bbedc598ffb7c5e029e74f87e2db945ea2
Augusto Corrêa <augustogatticorrea777@gmail.com>
2025-12-08 19:23:23 -0400

    refactor: improve comments and structure in get_db_connection function

--- backend/database/database.py
+++ backend/database/database.py
@@ -3,20 +3,24 @@ from contextlib import asynccontextmanager
 
 
 @asynccontextmanager
-async def get_db_connection():
+async def get_db_connection(): # Inicializador do banco de dados
     conn = None
     try:
+        # Tenta conectar-se ao banco de dados SQLite. 
         conn = await aiosqlite.connect("backend/database/holymind.db")
 
+        # Configura a fábrica de linhas (row_factory) para aiosqlite.Row.
+        # Isso permite que os resultados das consultas sejam acessados como 
+        # objetos que se comportam como dicionários (acesso por nome da coluna), 
+        # em vez de simples tuplas.
         conn.row_factory = aiosqlite.Row
 
+        # 'yield' é o ponto onde o bloco 'with' começa. 
+        # O objeto 'conn' é retornado para o 'as' do bloco 'with'.
         yield conn
-
-        
     finally:
+        # O bloco 'finally' garante que o código dentro dele será executado 
+        # mesmo se ocorrer uma exceção (erro) ou se o bloco 'with' for concluído.
         if conn:
+            # Fecha a conexão com o banco de dados de forma assíncrona.
             await conn.close()
-
-async def get_db():
-    async with get_db_connection() as conn:
-        yield conn
